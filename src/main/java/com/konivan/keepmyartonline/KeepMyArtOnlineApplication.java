f66b4b49ffbf91830085128a41954e2a5d0ce6ac
kot2d <kot2d@outlook.com>
2024-04-07 17:39:03 +0200

    first commit

---
+++ src/main/java/com/konivan/keepmyartonline/KeepMyArtOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.konivan.keepmyartonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepMyArtOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepMyArtOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konivan/keepmyartonline/KeepMyArtOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konivan.keepmyartonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepMyArtOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
