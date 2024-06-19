f602870d743e7cbd9874e61fe9e4e2585cb629e2
kot2d <kot2d@outlook.com>
2024-06-19 16:04:07 +0200

    added project info service

--- file-info-collector/src/main/java/org/example/Main.java
+++ file-info-collector/src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String dfdf = "<aaaaa>" +
+                "<bbbb>21323</bbbb>" +
+                "<cccc>343434</cccc>" +
+                "</aaaaa>";
+        String ree = "(<aaaaa>.*<cccc>)\\d+(</cccc>)";
+        dfdf = dfdf.replaceAll(ree, "$1333333$2");
+        System.out.println(dfdf);
     }
 }
