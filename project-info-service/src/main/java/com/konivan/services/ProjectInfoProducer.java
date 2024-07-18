be925dae50775410293be06c535463a5fc62fc57
kot2d <kot2d@outlook.com>
2024-07-18 21:58:51 +0200

    added kafka producer

--- project-info-service/src/main/java/com/konivan/services/ProjectInfoProducer.java
+++ project-info-service/src/main/java/com/konivan/services/ProjectInfoProducer.java
@@ -1,23 +1,34 @@
 package com.konivan.services;
 
 import com.konivan.domain.ProjectInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
+@Slf4j
 public class ProjectInfoProducer {
 
-    public void putNewProject(ProjectInfo projectInfo) {
+    public void putNewProject(ProjectInfo projectInfo) throws ExecutionException, InterruptedException {
 
         Properties props = new Properties();
 
         props.put("bootstrap.servers", "localhost:9092");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "com.konivan.services.ProjectInfoSerde");
         props.put("acks", "all");
         props.put("retries", "3");
         props.put("max.in.flight.requests.per.connection", "1");
 
-        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        KafkaProducer<String, ProjectInfo> producer = new KafkaProducer<>(props);
+        ProducerRecord<String, ProjectInfo> producerRecord =
+                new ProducerRecord<>("project-info",
+                        projectInfo.getId(),
+                        projectInfo);
+        RecordMetadata result = producer.send(producerRecord).get();
+        log.info("sussecc");
     }
 }
--- project-info-service/src/main/java/com/konivan/services/ProjectInfoRestController.java
+++ project-info-service/src/main/java/com/konivan/services/ProjectInfoRestController.java
@@ -3,22 +3,21 @@ package com.konivan.services;
 
 import com.konivan.domain.ProjectInfo;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.swing.plaf.PanelUI;
 import java.time.LocalDate;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 
 @RestController()
 @RequestMapping("project-info")
 public class ProjectInfoRestController {
 
     @PostMapping
-    public String postNewProjectInfo() {
-        ProjectInfo projectInfo = new ProjectInfo(LocalDate.now(), UUID.randomUUID().toString());
+    public void postNewProjectInfo(@RequestBody ProjectInfo projectInfo) throws ExecutionException, InterruptedException {
         new ProjectInfoProducer().putNewProject(projectInfo);
-        return "";
     }
 
 }
